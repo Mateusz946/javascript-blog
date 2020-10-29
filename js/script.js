c22a30d831e14c8ee0f8682b7f376dae0dd0b35c
Mateusz <mateusz079@wp.pl>
2020-10-29 23:28:55 +0100

    Add code js for add active class

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function titleClickHandler(event){
+  const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event)
+  console.log(event);
+  
 
   /* remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -10,6 +12,8 @@ function titleClickHandler(event){
       activeLink.classList.remove('active');
   }
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll('.active');
